007eb69b11324336261f1e7b9b2d62e67b4beb2c
felix <sfx86@users.noreply.github.com>
2023-07-19 15:44:19 -0400

    feat: water main data source check
    new quality control check and some refactoring

--- ColaWater/tools/qc.py
+++ ColaWater/tools/qc.py
@@ -1,5 +1,4 @@
 import arcpy
-from functools import cache
 from pathlib import Path
 import re
 import utils
@@ -10,18 +9,24 @@ def execute(parameters: list[arcpy.Parameter]) -> None:
     arcpy.SetProgressor("default", "Starting quality control checks...")
     status = utils.StatusUpdater()
     summaries = utils.SummaryContainer()
-    summaries.add_summaries(["validator", "int_file"])
+    summaries.add_summaries(["fid_format", "wm_file", "wm_datasource"])
 
-    checks = parameters[:2]
-    lyrs = parameters[2:]
-    is_fid_check = checks[0].value
-    is_wm_check = checks[1].value
+    LAYER_START = 3
+    checks = parameters[:LAYER_START]
+    lyrs = parameters[LAYER_START:]
+    lyr_wm = lyrs[-1]
+    is_fid_format_check = checks[0].value
+    is_wm_file_check = checks[1].value
+    is_ds_check = checks[2].value
 
-    if is_fid_check:
-        _fid_validation_qc(lyrs, status, summaries)
+    if is_fid_format_check:
+        _fid_format_qc(lyrs, status, summaries)
 
-    if is_wm_check:
-        _wm_assoc_file_qc(lyrs[-1], status, summaries)
+    if is_wm_file_check:
+        _wm_assoc_file_qc(lyr_wm, status, summaries)
+
+    if is_ds_check:
+        _wm_datasource_qc(lyr_wm, status, summaries)
 
     # TODO: deduplication
     # TODO: domain conformation
@@ -37,8 +42,10 @@ def post_execute(parameters: list[arcpy.Parameter]) -> None:
 def parameters() -> list[arcpy.Parameter]:
     """Return a list of parameter definitions."""
     check_templates = (
-        ("fid_check", "Validate facility identifiers"),
-        ("wm_check", "Verify associated files for integrated mains"),
+        # make sure to increment LAYER_START if adding a check here
+        ("fid_check", "Check facility identifiers"),
+        ("wm_file_check", "Check water main files"),
+        ("wm_datasource_check", "Check water main data sources"),
     )
 
     checks = [
@@ -87,12 +94,12 @@ def update_messages(parameters: list[arcpy.Parameter]) -> None:
     pass
 
 
-def _fid_validation_qc(
+def _fid_format_qc(
     layers: list[arcpy._mp.Layer],
     status: utils.StatusUpdater,
     summaries: utils.SummaryContainer,
 ) -> None:
-    """Finds all non-conforming facility identifiers and generates a report containing them."""
+    """Finds all incorrectly formatted facility identifiers."""
     arcpy.SetProgressor("step", "Validating facility identifiers...", 0, 7)
     # regexes correspond 1:1 with layer parameters
     regexes = (
@@ -119,21 +126,23 @@ def _fid_validation_qc(
         lyr_name = l.valueAsText
         lyr_path = utils.get_layer_path(lyr)
 
-        # fid validation
-        status.update_info(f"Validating facility identifiers for [{lyr_name}]...")
+        status.update_info(
+            f"Finding incorrectly formatted facility identifiers in [{lyr_name}]..."
+        )
+
         try:
             with arcpy.da.SearchCursor(lyr_path, fields) as cursor:
-                summaries.validator.add_header(
-                    f"[{lyr_name}] Non-conforming facility identifiers (object ID, facility identifier):"
+                summaries.fid_format.add_header(
+                    f"[{lyr_name}] Incorrectly formatted facility identifiers (object ID, facility identifier):"
                 )
                 for row in cursor:
                     oid, fid = row
-                    if not r.fullmatch(fid):
-                        summaries.validator.add_item(f"{oid}, {fid}")
+                    if not r.fullmatch(str(fid)):
+                        summaries.fid_format.add_item(f"{oid}, {fid}")
         # arcpy should only ever throw RuntimeError here, but you never know
         except Exception:
             # post existing summaries as to not lose information
-            summaries.post()
+            summaries.post(dumped=True)
             status.update_err(utils.RUNTIME_ERROR_MSG)
 
 
@@ -163,70 +172,104 @@ def _wm_assoc_file_qc(
     fields = ("OBJECTID", "COMMENTS")
     lyr_disp_name = water_main_layer.displayName
     lyr_path = utils.get_layer_path(lyr)
+    num_not_exists = 0
+    num_exists = 0
+    unique_comments = set()
     where_integrated = "INTEGRATIONSTATUS = 'Y'"
+
+    summaries.wm_file.add_header(
+        f"[{lyr_name}] Non-existant associated files (object ID, comments):"
+    )
+    summaries.wm_file.add_header(utils.CSV_PROCESSING_MSG)
 
     try:
         with arcpy.da.SearchCursor(lyr_path, fields, where_integrated) as cursor:
-            summaries.int_file.add_header(
-                f"[{lyr_name}] Invalid associated files (object ID, comments):"
-            )
-            summaries.int_file.add_header(
-                f"Note: commas and quotation marks have been removed so this output can be consumed properly as a CSV."
-            )
-            num_valid = 0
-            num_invalid = 0
             for row in cursor:
                 oid = row[0]
-                comments = row[1]
-                # only check for none as empty strings are also falsy and
-                # naming those <Null> would cause confusion
-                if comments is None:
-                    comments = "<Null>"
-                    summaries.int_file.add_item(f"{oid}, {comments}")
-                    num_invalid += 1
+                comments = utils.process_nullable_attr(row[1], csv=True)
+                unique_comments.add(comments)
+
+                if comments == "<Null>":
+                    summaries.wm_file.add_item(f"{oid}, {comments}")
+                    num_not_exists += 1
                     continue
+
+                if utils.is_existing_scan(comments):
+                    num_exists += 1
                 else:
-                    # some comments have erroneous whitespace or contain commas or quotes
-                    # remove these to make use as csv possible
-                    comments = comments.strip().replace(",", "").replace('"', "")
-                if _is_valid_file(comments):
-                    num_valid += 1
-                else:
-                    summaries.int_file.add_item(f"{oid}, {comments}")
-                    num_invalid += 1
+                    summaries.wm_file.add_item(f"{oid}, {comments}")
+                    num_not_exists += 1
                 # there's so many files that the progressor only needs updating
-                # every couple of files; the progressor queues the updates
-                # anyways, so might as well not hit it as often
-                # ~3k files per second, so total mod 1500 will update ~2x/s 
-                if (num_valid + num_invalid) % 1500 == 0:
-                    arcpy.SetProgressorLabel(
-                        f"Associated file count (valid : invalid): {num_valid:>9n} : {num_invalid:<9n}"
+                # every couple of files
+                # the progressor queues the updates anyways,
+                # so might as well not hit it as often
+                # ~3k files per second, so (total mod 1500) will update ~2x/s
+                if (num_exists + num_not_exists) % 1500 == 0:
+                    status.update_label(
+                        f"Associated file count (existant : non-existant): {num_exists:>9n} : {num_not_exists:<9n}"
                     )
     # arcpy should only ever throw RuntimeError here, but you never know
     except Exception:
         # post existing summaries as to not lose information
-        summaries.post()
+        summaries.post(dumped=True)
         status.update_err(utils.RUNTIME_ERROR_MSG)
 
-    summaries.int_file.add_header(
+    summaries.wm_file.add_header(
         f"[{lyr_name}] Verified associated files for integrated mains:"
     )
-    summaries.int_file.add_item(f"{num_valid:n} valid, {num_invalid:} invalid.")
-    summaries.int_file.add_item(f"{num_valid + num_invalid:n} total files checked.")
-
-
-@cache
-def _is_valid_file(filename: str) -> bool:
-    """Return true if the given string is a valid scan filename."""
-    # takes advantage of short-circuiting to avoid filesystem ops where possible
-    # this function is extremely hot and many of the arguments are identical, hence
-    # the @cache decorator
-    return filename and (
-        (
-            # valid data sources only ever have these extensions
-            filename.endswith(".tif")
-            or filename.endswith(".pdf")
-            or filename.endswith(".dwg")
-        )
-        and Path(f"M:\\Util&Eng\\Dept_Staff\\Scans\\{filename}").exists()
+    summaries.wm_file.add_item(
+        f"{num_exists:n} existant, {num_not_exists:} non-existant."
     )
+    summaries.wm_file.add_item(f"{len(unique_comments):n} unique non-existant files.")
+    summaries.wm_file.add_item(f"{num_exists + num_not_exists:n} total files checked.")
+
+
+def _wm_datasource_qc(
+    water_main_layer: arcpy._mp.Layer,
+    status: utils.StatusUpdater,
+    summaries: utils.SummaryContainer,
+) -> None:
+    """Verifies that each integrated main's data source is set and not Unknown."""
+    arcpy.SetProgressor("default", "Verifying data sources for integrated mains...")
+
+    lyr = water_main_layer.value
+    lyr_name = water_main_layer.valueAsText
+
+    status.update_info(
+        f"Verifying data sources for integrated mains in [{lyr_name}]...",
+        bump=False,
+    )
+
+    # guard against None
+    if not lyr:
+        status.update_warn(f"Layer omitted: {lyr_disp_name}", bump=False)
+        return
+
+    fields = ("OBJECTID", "DATASOURCE")
+    lyr_disp_name = water_main_layer.displayName
+    lyr_path = utils.get_layer_path(lyr)
+    num_missing_unk = 0
+    where_wrong = "INTEGRATIONSTATUS = 'Y' AND (DATASOURCE = 'UNK' OR DATASOURCE = '' OR DATASOURCE IS NULL)"
+
+    summaries.wm_datasource.add_header(
+        f"[{lyr_name}] Missing or unknown data sources (object ID, datasource):"
+    )
+    summaries.wm_datasource.add_header(utils.CSV_PROCESSING_MSG)
+
+    try:
+        with arcpy.da.SearchCursor(lyr_path, fields, where_wrong) as cursor:
+            for row in cursor:
+                oid = row[0]
+                datasource = utils.process_nullable_attr(row[1], csv=True)
+                summaries.wm_datasource.add_item(f"{oid}, {datasource}")
+                num_missing_unk += 1
+    # arcpy should only ever throw RuntimeError here, but you never know
+    except Exception:
+        # post existing summaries as to not lose information
+        summaries.post(dumped=True)
+        status.update_err(utils.RUNTIME_ERROR_MSG)
+
+    summaries.wm_datasource.add_header(
+        f"[{lyr_name}] Missing or unknown data sources for integrated mains:"
+    )
+    summaries.wm_datasource.add_item(f"{num_missing_unk:n} missing or unknown.")
