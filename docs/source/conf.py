943e3dfc4d6987ecb03304ce193521005fcde28b
felix <sfx86@users.noreply.github.com>
2023-07-26 09:51:47 -0400

    docs: clean up sphinx conf
    removed boilerplate comments and added more specific ones, removed erroneous auto-import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,49 +1,38 @@
 import os
 import sys
-from re import template
 
+# make sure sphinx has right source path
 sys.path.insert(0, os.path.abspath("../../src"))
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# project info
 project = "ColaWaterToolbox"
 copyright = "2023, Columbia Water"
 author = "Columbia Water"
 release = "2.3.0"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# general config
 extensions = [
     "autoapi.extension",
     "sphinx.ext.napoleon",
 ]
-
 templates_path = ["_templates"]
 exclude_patterns = []
 
+# napoleon
 napoleon_numpy_docstring = False
-
+# autoapi
 autoapi_dirs = ["../../src"]
 autoapi_root = "api-reference"
 autoapi_template_dir = f"{templates_path[0]}/_autoapi_templates"
-
+# include shared links at the end of every file
 rst_epilog = ".. include:: /include/links.rst"
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# theme
 html_theme = "furo"
 html_static_path = ["_static"]
 
 project_tagline = "ArcGIS geoprocessing tools for Columbia Water"
 html_title = f"{project} {release} <br><small>{project_tagline}</small>"
-
 
 html_theme_options = {
     "navigation_with_keys": True,
