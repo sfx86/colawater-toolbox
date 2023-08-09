3d717bce9c7625d9851eeac9a7d3a38ba13c21d3
felix <sfx86@users.noreply.github.com>
2023-08-09 10:24:49 -0400

    templates: update toolbox
    bring templates up to date with current toolbox formatting

--- templates/toolbox.py
+++ templates/toolbox.py
@@ -1,41 +1,30 @@
+from typing import Any
+
 import arcpy
 
-from colawater import tools
+from colawater.tools import toolname
 
 
 class Tool:
-    """
-    Contains metadata and methods for the {} tool.
-
-    Attributes:
-        label (str): The tool label.
-        description (str): The tool description.
-        canRunInBackground (bool): Whether the tool can run in the background.
-
-    Note:
-        Implementation details and method descriptions can be found in the
-        documentation for ``colawater.tools.{}``.
-    """
-
     def __init__(self) -> None:
         self.label = ""
         self.description = ""
         self.canRunInBackground = False
 
     def getParameterInfo(self) -> list[arcpy.Parameter]:
-        return tools.tool.parameters()
+        return toolname.parameters()
 
     def isLicensed(self) -> bool:
         return True
 
     def updateParameters(self, parameters: list[arcpy.Parameter]) -> None:
-        tools.tool.update_parameters(parameters)
+        toolname.update_parameters(parameters)
 
     def updateMessages(self, parameters: list[arcpy.Parameter]) -> None:
-        tools.tool.update_messages(parameters)
+        toolname.update_messages(parameters)
 
     def execute(self, parameters: list[arcpy.Parameter], messages: Any) -> None:
-        tools.tool.execute(parameters)
+        toolname.execute(parameters)
 
     def postExecute(self, parameters: list[arcpy.Parameter]) -> None:
-        tools.quality_control.post_execute(parameters)
+        toolname.post_execute(parameters)
