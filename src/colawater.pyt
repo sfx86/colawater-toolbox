bab040df0d3d4bddb3940fdaff1eea81138f5994
felix <sfx86@users.noreply.github.com>
2023-09-28 15:53:33 -0400

    refactor(tool): change toolshed defaults and add docstring

--- src/colawater.pyt
+++ src/colawater.pyt
@@ -16,20 +16,20 @@ class Toolbox:
 AppendToART = toolshed(
     "Append to ART",
     "Appends new integrated mains to the Asset Reference Table.",
-    parameters=append_to_art.parameters,
-    execute=append_to_art.execute,
+    append_to_art.parameters,
+    append_to_art.execute,
 )
 
 CalculateFacilityIdentifiers = toolshed(
     "Calculate Facility Identifiers",
     "Calculates FIDs and FID indices for specified water layers.",
-    parameters=calculate_fids.parameters,
-    execute=calculate_fids.execute,
+    calculate_fids.parameters,
+    calculate_fids.execute,
 )
 
 WaterQualityControl = toolshed(
     "Water Quality Control",
     "Executes selected quality control checks on specified water layers.",
-    parameters=quality_control.parameters,
-    execute=quality_control.execute,
+    quality_control.parameters,
+    quality_control.execute,
 )
--- src/colawater/lib/tool.py
+++ src/colawater/lib/tool.py
@@ -7,8 +7,8 @@ Examples:
         Foo = toolshed(
             "Foo",
             "Performs arbitrary incantations on your database.",
-            parameters=some_module.parameters,
-            execute=some_module.execute,
+            some_module.parameters,
+            some_module.execute,
         )
 
 """
@@ -20,14 +20,40 @@ import arcpy
 def toolshed(
     label: str,
     description: str,
+    parameters: Callable[[], list[arcpy.Parameter]],
+    execute: Callable[[list[arcpy.Parameter]], None],
     category: Optional[str] = None,
     backgroundable: bool = False,
-    parameters: Callable[[], list[arcpy.Parameter]] = lambda: [],
     update_parameters: Callable[[list[arcpy.Parameter]], None] = lambda _: None,
     update_messages: Callable[[list[arcpy.Parameter]], None] = lambda _: None,
-    execute: Callable[[list[arcpy.Parameter]], None] = lambda _: None,
     post_execute: Callable[[list[arcpy.Parameter]], None] = lambda _: None,
 ) -> type:
+    """
+    Returns a tool class in the shape expected by ArcPy.
+
+    Arguments:
+        label (str):
+        description (str):
+        parameters (Callable[[], list[arcpy.Parameter]]): A function providing the tool parameters.
+        execute (Callable[[list[arcpy.Parameter]], None]): The tool's entry point.
+        category (Optional[str]):
+            A tool category. Defaults to None.
+        backgroundable (bool):
+            Whether the tool can be backgrounded. Defaults to False.
+        update_parameters (Callable[[list[arcpy.Parameter]], None]):
+            A function that updates the parameter values.
+            Defaults to ``lambda _: None``.
+        update_messages (Callable[[list[arcpy.Parameter]], None]):
+            A function that updates parameter messages.
+            Defaults to ``lambda _: None``.
+        post_execute (Callable[[list[arcpy.Parameter]], None]):
+            A function to be run after execute runs.
+            Defaults to ``lambda _: None``.
+
+    Returns:
+        type: A class with all the required attributes and methods for use in a toolbox.
+    """
+
     class Tool:
         def __init__(
             self,
