14400f22a06dd026ee0665bd852d3621ddc5dc54
felix <sfx86@users.noreply.github.com>
2023-12-14 14:25:50 -0500

    feat!(attribute): remove csv flag

--- src/colawater/lib/attribute.py
+++ src/colawater/lib/attribute.py
@@ -6,36 +6,30 @@ Examples:
 
         foo = None
         foo = process(foo) # Returns "<Null>".
+        
+        bar = "      whitespace example   \n\n"
+        bar = process(bar) # Returns "whitespace example"
 """
 from typing import Any, Optional
 
-CSV_PROCESSING_MSG: str = "Commas, leading and trailing whitespace, and quotation marks have been removed so this output can be consumed properly as a CSV."
-"""
-Message text to display when the following output has been modified for CSV use.
-"""
 
-
-def process(attr: Optional[Any], csv: bool = False) -> str:
+def process(attr: Optional[Any]) -> str:
     """
     Returns a human readable string representation of a nullable field value.
 
-    Turns a ``None`` value into ``"<Null>"`` and optionally strips leading and trailing whitespace,
-    commas, and quotation marks for ease of use as a CSV.
+    Turns a ``None`` value into ``"<Null>"`` and optionally strips leading and trailing whitespace.
 
     Arguments:
-        attr (Optional[Union[Any]]): The attribute value to be processed.
-        csv (bool): Whether to apply CSV pre-processing.
+        attr (Optional[Any]): The attribute value to be processed.
 
     Returns:
         str: The processed attribute value.
     """
-    # only check for none as empty strings are also falsy and
-    # naming those <Null> would cause confusion
+    # only check for none as empty strings are also falsy,
+    # so naming those <Null> would be incorrect
     if attr is None:
-        return "<Null>"
+        processed_attr = "<Null>"
+    else:
+        processed_attr = str(attr).strip()
 
-    attr_str = str(attr)
-    if csv:
-        return attr_str.strip().replace(",", "").replace('"', "")
-
-    return attr_str
+    return processed_attr
