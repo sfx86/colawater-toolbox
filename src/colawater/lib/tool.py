6a6a12492f04e4938142a9a3e033da365d2d2765
felix <sfx86@users.noreply.github.com>
2024-04-02 16:11:39 -0400

    refactor(tool): add new categories and rename entry to main

--- src/colawater/lib/tool.py
+++ src/colawater/lib/tool.py
@@ -9,6 +9,7 @@ Examples:
         Foo = toolshed(Wizard)
 
 """
+
 from enum import Enum, unique
 from functools import wraps
 from typing import Any, Callable, TypeVar
@@ -18,31 +19,9 @@ import arcpy
 
 @unique
 class Category(Enum):
-    CheckIn = "Check-in"
-    Tools = "Tools"
-    QualityControl = "Quality Control"
-
-
-_T = TypeVar("_T")
-
-
-def entry(tool_name: str) -> Callable[[Callable[..., _T]], Callable[..., _T]]:
-    """
-    Wraps the decorated function and prefixes it with boilerplate progressor setup.
-
-    Arguments:
-        tool_name (str): The tool name of the entry point being annotated.
-    """
-
-    def decorator(f: Callable[..., _T]) -> Callable[..., _T]:
-        @wraps(f)
-        def wrapper(*args: Any, **kwargs: Any) -> _T:
-            arcpy.SetProgressor("default", f"Running {tool_name}")
-            return f(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
+    CheckIn = "Check-in Tools"
+    CheckOut = "Checkout Tools"
+    QualityControl = "Quality Control Tools"
 
 
 def label(name: str, category: Category) -> str:
@@ -57,3 +36,25 @@ def label(name: str, category: Category) -> str:
         str: The formatted name.
     """
     return f"{name} ({category.value})"
+
+
+_T = TypeVar("_T")
+
+
+def main(tool_name: str) -> Callable[[Callable[..., _T]], Callable[..., _T]]:
+    """
+    Decorates the tool entry point function and prefixes it with boilerplate progressor setup.
+
+    Arguments:
+        tool_name (str): The tool name of the entry point being annotated.
+    """
+
+    def decorator(f: Callable[..., _T]) -> Callable[..., _T]:
+        @wraps(f)
+        def wrapper(*args: Any, **kwargs: Any) -> _T:
+            arcpy.SetProgressor("default", f"Running {tool_name}")
+            return f(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
