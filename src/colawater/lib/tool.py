ad3e9e6fd2b21568fe7df5f320c5789a5aa3d2f4
felix <sfx86@users.noreply.github.com>
2024-05-09 10:48:35 -0400

    refactor: slim tool

--- src/colawater/lib/tool.py
+++ src/colawater/lib/tool.py
@@ -19,42 +19,7 @@ import arcpy
 
 @unique
 class Category(Enum):
+    ArcGISOnline = "ArcGIS Online Tools"
     CheckIn = "Check-in Tools"
     CheckOut = "Checkout Tools"
     QualityControl = "Quality Control Tools"
-
-
-def label(name: str, category: Category) -> str:
-    """
-    Formats a tool label.
-
-    Arguments:
-        name (str): The tool name.
-        category (Category): The tool category.
-
-    Returns:
-        str: The formatted name.
-    """
-    return f"{name} ({category.value})"
-
-
-_T = TypeVar("_T")
-
-
-def main(tool_name: str) -> Callable[[Callable[..., _T]], Callable[..., _T]]:
-    """
-    Decorates the tool entry point function and prefixes it with boilerplate progressor setup.
-
-    Arguments:
-        tool_name (str): The tool name of the entry point being annotated.
-    """
-
-    def decorator(f: Callable[..., _T]) -> Callable[..., _T]:
-        @wraps(f)
-        def wrapper(*args: Any, **kwargs: Any) -> _T:
-            arcpy.SetProgressor("default", f"Running {tool_name}")
-            return f(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
