c7c9002cfbd33e2886943fd126d8e277b0a648b7
felix <sfx86@users.noreply.github.com>
2024-05-14 10:43:42 -0400

    prelude: remove prelude

--- src/colawater/toolbox/prelude.py
+++
@@ -1,5 +0,0 @@
-from colawater.toolbox.append_to_art.tool import AppendToART
-from colawater.toolbox.calculate_fids.tool import CalculateFacIDs
-from colawater.toolbox.dump_to_csv.tool import DumpToCSV
-from colawater.toolbox.quality_control.tool import QualityControl
-from colawater.toolbox.update_ago_data.tool import UpdateAGOData
