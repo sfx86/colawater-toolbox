5d1afcaa30ca983ee7141c61ec6a484562dc9d08
felix <sfx86@users.noreply.github.com>
2024-05-30 15:00:21 -0400

    refactor(release): simplify release script and add calver format

--- release.ps1
+++ release.ps1
@@ -1,24 +1,9 @@
-param (
-    $version
-)
-
 $git_dir = "$(git rev-parse --show-toplevel)"
 $pyproject = "${git_dir}/pyproject.toml"
 $changelog = "${git_dir}/CHANGELOG.md"
 $history = "${git_dir}/HISTORY.md"
 
-# check if pyproject path exists
-If (-Not (Test-Path ${pyproject} -PathType Leaf)) {
-    throw [System.IO.FileNotFoundException] "Provided pyproject.toml does not exist: '${pyproject}'"
-}
-# check for git-cliff
-If (-Not (git cliff --version)) {
-    throw [System.Management.Automation.CommandNotFoundException] "git-cliff not found. Make sure the virtual environment is activated."
-}
-# ensure version input & correct format
-While (${version} -notmatch '^v[0-9]+\.[0-9]+\.[0-9]+$') {
-    $version = Read-Host -Prompt "Enter a version tag (vX.Y.Z)"  
-}
+$version = Read-Host -Prompt "Enter a version tag (0Y.0W.MICRO)"  
 
 # update version in pyproject.toml
 $msg = "# managed by ./release.ps1"
@@ -32,7 +17,6 @@ git cliff --tag "${version}" --config ${pyproject} > ${history}
 git add -A
 git commit -m "chore(release): prepare for $version"
 git tag "${version}"
-
 
 Write-Host "All done :)"
 Write-Host "Now ``git push`` and ``git push --tags"
